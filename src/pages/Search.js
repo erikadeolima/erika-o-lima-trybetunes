7d57aa02ff5be9dfd31ef0c9f535ab9a415cf67d
Erika de Oliveira Lima <erikadeo.lima@hotmail.com>
2022-05-20 19:33:58 -0300

    requisito 5: concluido

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,22 +2,49 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    artistName: '',
+    isLoginButtonDisabled: true,
+  };
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState(() => ({ [name]: value }), this.validaButton);
+  };
+
+  validaButton = () => {
+    const {
+      artistName,
+    } = this.state;
+    this.setState({ isLoginButtonDisabled: artistName.length < '2' });
+  };
+
   render() {
+    const {
+      artistName,
+      isLoginButtonDisabled,
+    } = this.state;
     return (
       <div>
-        <header>
-          <Header />
-        </header>
         <div data-testid="page-search" type="text">
+          <header>
+            <Header />
+          </header>
           <form>
             <input
-              name="searchAlbum"
+              name="artistName"
               type="text"
-              data-testid="search-album-input"
-              value /* ={ searchAlbum } */
-              onChange
+              data-testid="search-artist-input"
+              value={ artistName }
+              onChange={ this.onInputChange }
             />
-            <button type="submit" data-testid="search-button">Pesquisar</button>
+            <button
+              type="submit"
+              data-testid="search-artist-button"
+              disabled={ isLoginButtonDisabled }
+            >
+              Pesquisar
+            </button>
           </form>
         </div>
       </div>
